fd088d32b7fd2c4937bf2ec8bf7c2dc97bf1ac8e
ParteekPa <parteek.parteek@kuonitumlare.com>
2024-06-30 11:40:44 +0530

    Linked List Implementation

---
+++ src/com/dataStructure/linkedList/Main.java
@@ -0,0 +1,10 @@
+package com.dataStructure.linkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        Node head=new Node(1);
+        head.next=new Node(2);
+        head.next.next=new Node(3);
+
+    }
+}
---
+++ src/com/dataStructure/linkedList/Node.java
@@ -0,0 +1,10 @@
+package com.dataStructure.linkedList;
+
+public class Node {
+    public int data;
+    public Node next;
+    Node(int data){
+        this.data =data;
+        this.next=null;
+    }
+}
