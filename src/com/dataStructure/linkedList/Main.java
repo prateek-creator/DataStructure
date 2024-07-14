9ff3e24600995791534ee96102e932081dab2716
ParteekPa <parteek.parteek@kuonitumlare.com>
2024-07-14 16:29:28 +0530

    added insertion or deletion functions

--- src/com/dataStructure/linkedList/Main.java
+++ src/com/dataStructure/linkedList/Main.java
@@ -6,5 +6,13 @@ public class Main {
         head.next=new Node(2);
         head.next.next=new Node(3);
         Node.printList(head);
+        head=Node.insertAtPos(head,1,10);
+        Node.printList(head);
+        head=Node.insertAtHead(head,12);
+        Node.printList(head);
+        head=Node.insertAtTail(head,4);
+        Node.printList(head);
+        head=Node.deleteLastNode(head);
+        Node.printList(head);
     }
 }
--- src/com/dataStructure/linkedList/Node.java
+++ src/com/dataStructure/linkedList/Node.java
@@ -12,14 +12,15 @@ public class Node {
             System.out.print(head.data+" ");
             head=head.next;
         }
+        System.out.println();
     }
-    public Node insertAtHead(Node head, int data){
+    public static Node insertAtHead(Node head, int data){
 
         Node newNode = new Node(data);
         newNode.next = head;
         return newNode;
     }
-    public Node insertAtTail(Node head,int data){
+    public static Node insertAtTail(Node head,int data){
         Node newNode = new Node(data);
         if(head==null){
             return newNode;
@@ -32,7 +33,7 @@ public class Node {
         return head;
 
     }
-    public Node insertAtPos(Node head, int pos, int data){
+    public static Node insertAtPos(Node head, int pos, int data){
         Node tempNode=new Node(data);
         if(pos==1){
             tempNode.next=head;
@@ -48,4 +49,41 @@ public class Node {
         return head;
 
     }
+    public static Node deleteFirstNode(Node head){
+        Node newHead=head.next;
+        head.next=null;
+        return newHead;
+    }
+    public static Node deleteLastNode(Node head){
+        if(head==null){
+            return null;
+        }
+        if(head.next==null){
+            return null;
+        }
+        Node curr=head;
+        while(curr.next.next!=null){
+            curr=curr.next;
+        }
+        curr.next=null;
+        return head;
+    }
+    public static int search(Node head,int data){
+        if(head==null){
+            return -1;
+        }
+        int pos=1;
+        Node curr=head;
+        while(curr!=null){
+            if(curr.data==data){
+                return pos;
+            }
+            else{
+                pos++;
+                curr=curr.next;
+            }
+        }
+        return -1;
+    }
+
 }
